baf66698af781915532c34b542e91b1f32c0a12b
agustinrobledo <agustinrobledo59.ar@gmail.com>
2023-03-25 12:50:20 -0300

    accordion in addtask

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,44 +1,26 @@
-import { useState } from "react"
+import TaskForm from "./TaskForm"
+import * as Accordion from "@radix-ui/react-accordion"
 
 interface propsTask {
     addTask: (t: string) => void
 }
 
 const NewTask = ({ addTask }: propsTask) => {
-    const [task, setTask] = useState("")
-
-    const handleSubmit = (e: React.FormEvent, t: string) => {
-        e.preventDefault()
-        addTask(t)
-        setTask("")
-    }
-
     return (
-        <form className="flex w-3/4 justify-between">
-            <label
-                htmlFor="task"
-                className="focus group relative flex h-full w-4/6 flex-col-reverse"
-            >
-                <input
-                    type="text"
-                    placeholder="Study English..."
-                    onChange={(e) => setTask(e.target.value)}
-                    value={task}
-                    className="w-full rounded-lg border-4 p-4 leading-9 text-dark-green placeholder:text-blue focus:outline-none"
-                />
-            </label>
-            <button
-                className="group relative inline-block w-1/5 rounded-lg  bg-slate-200"
-                type="submit"
-                onClick={(e) => handleSubmit(e, task)}
-            >
-                <span className="absolute inset-0 h-full w-full translate-x-1 translate-y-1 transform rounded-lg bg-black transition duration-200 ease-out group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
-                <span className="absolute inset-0 h-full w-full rounded-lg border-2 border-black bg-white group-hover:bg-white"></span>
-                <span className="relative rounded-lg text-black group-hover:text-black">
-                    <span className="font-semibold">add</span>
-                </span>
-            </button>
-        </form>
+        <div className="flex w-full justify-center">
+            <Accordion.Root type="single">
+                <Accordion.Item value="item-1">
+                    <Accordion.Header className="flex justify-center">
+                        <Accordion.Trigger value="item-1">
+                            Add new task
+                        </Accordion.Trigger>
+                    </Accordion.Header>
+                    <Accordion.Content>
+                        <TaskForm addTask={addTask} />
+                    </Accordion.Content>
+                </Accordion.Item>
+            </Accordion.Root>
+        </div>
     )
 }
 
---
+++ src/components/TaskForm.tsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react"
+
+interface taskFormProps {
+    addTask: (t: string) => void
+}
+const TaskForm = ({ addTask }: taskFormProps) => {
+    const [task, setTask] = useState("")
+    const handleSubmit = (e: React.FormEvent, t: string) => {
+        e.preventDefault()
+        addTask(t)
+        setTask("")
+    }
+
+    return (
+        <form className="flex h-40 w-full flex-col items-center justify-center gap-4 bg-red align-middle">
+            <label htmlFor="task" className="w-4/6 ">
+                <input
+                    type="text"
+                    placeholder="Study English..."
+                    onChange={(e) => setTask(e.target.value)}
+                    value={task}
+                    className="w-full rounded-lg border-4 p-4 leading-9 text-dark-green placeholder:text-blue focus:outline-none"
+                />
+            </label>
+            <div className="flex ">
+                <label>Time</label>
+                <input type="number" name="minutes" />
+                <input type="number" name="seconds" />
+                <button
+                    className="group relative inline-block w-1/5 rounded-lg  bg-slate-200"
+                    type="submit"
+                    onClick={(e) => handleSubmit(e, task)}
+                >
+                    <span className="absolute inset-0 h-full w-full translate-x-1 translate-y-1 transform rounded-lg bg-black transition duration-200 ease-out group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
+                    <span className="absolute inset-0 h-full w-full rounded-lg border-2 border-black bg-white group-hover:bg-white"></span>
+                    <span className="relative rounded-lg text-black group-hover:text-black">
+                        <span className="font-semibold">add</span>
+                    </span>
+                </button>
+            </div>
+        </form>
+    )
+}
+
+export default TaskForm
