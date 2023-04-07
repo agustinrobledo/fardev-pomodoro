520bfbb7944b6615def340cbef6b3219100e5e14
agustinrobledo <agustinrobledo59.ar@gmail.com>
2023-04-07 14:25:24 -0300

    sidebar styled

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,22 @@ import { useState } from "react"
 import Sidebar from "./Sidebar"
 
 const Navbar = () => {
-    const [onShow, setOnShow] = useState(false)
+    const [onShow, setOnShow] = useState<boolean>(false)
+
+    const handleShow = () => {
+        setOnShow(!onShow)
+    }
     return (
         <>
             {onShow ? (
-                <Sidebar />
+                <Sidebar onShow={onShow} setOnShow={handleShow} />
             ) : (
                 <div className="flex h-14 justify-between border">
                     <div className="flex items-center justify-center p-4">
                         <h1>FARdev Pomodoro</h1>
                     </div>
                     <div className="flex w-10 items-center justify-center border">
-                        <button onClick={() => setOnShow(true)}>/.</button>
+                        <button onClick={handleShow}>/.</button>
                     </div>
                 </div>
             )}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,10 +1,60 @@
-import React from "react"
+import React, { useLayoutEffect, useRef } from "react"
+import { gsap } from "gsap"
 
-const Sidebar = () => {
+interface sidebarProps {
+    setOnShow: () => void
+}
+
+const Sidebar = ({ setOnShow }: sidebarProps) => {
+    const linksRef = useRef<HTMLDivElement>(null)
+
+    useLayoutEffect(() => {
+        if (linksRef.current) {
+            let ctx = gsap.context(() => {
+                gsap.from(".link", {
+                    y: -100,
+                    stagger: 0.3,
+                    opacity: 0,
+                })
+            }, linksRef)
+            return () => ctx.revert()
+        }
+    }, [])
     return (
-        <div>
-            <div>
-                <a href="#">github</a>
+        <div className="absolute z-10 flex h-screen w-screen flex-col bg-black">
+            <div className="flex h-14 justify-between border">
+                <div className="flex items-center justify-center p-4">
+                    <h1>FARdev Pomodoro</h1>
+                </div>
+                <div className="flex w-10 items-center justify-center border">
+                    <button onClick={setOnShow}>/.</button>
+                </div>
+            </div>
+            <div
+                ref={linksRef}
+                className="flex h-5/6 flex-col items-center justify-center gap-6 text-4xl"
+            >
+                <a
+                    className="link"
+                    target="_blank"
+                    href="https://github.com/agustinrobledo"
+                >
+                    github
+                </a>
+                <a
+                    className="link"
+                    target="_blank"
+                    href="https://twitter.com/_FARdev"
+                >
+                    twitter
+                </a>
+                <a
+                    className="link"
+                    target="_blank"
+                    href="https://agustinrobledo.com"
+                >
+                    portfolio
+                </a>
             </div>
         </div>
     )
